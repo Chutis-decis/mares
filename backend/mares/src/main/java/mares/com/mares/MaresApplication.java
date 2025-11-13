f508166cb675677e3932afd83444c8b4983c9147
chutis <L191200090@tlalpan.tecnm.mx>
2025-11-12 20:37:41 -0600

    added the backend

---
+++ backend/mares/src/main/java/mares/com/mares/MaresApplication.java
@@ -0,0 +1,13 @@
+package mares.com.mares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaresApplication.class, args);
+	}
+
+}
---
+++ backend/mares/src/test/java/mares/com/mares/MaresApplicationTests.java
@@ -0,0 +1,13 @@
+package mares.com.mares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
